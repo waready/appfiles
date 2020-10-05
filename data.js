92d0b102a38657761d416a1c5da69f1b08b74265
waready <elbarto119@hotmail.com>
2020-10-05 15:49:21 -0500

    frist commit

---
+++ data.js
@@ -0,0 +1,56 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-r0010193_20201002160628",
+      "name": "R0010193_20201002160628",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.5172443264631283,
+        "pitch": 0.23212571669361992,
+        "fov": 1.4612050697485615
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7563278488295104,
+          "pitch": -0.04353189836034943,
+          "rotation": 0,
+          "target": "0-r0010193_20201002160628"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.5184599539403028,
+          "pitch": 0.6166983922029861,
+          "title": "PC",
+          "text": "PC PC"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
